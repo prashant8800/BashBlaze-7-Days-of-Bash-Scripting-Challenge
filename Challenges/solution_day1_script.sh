58799192750e1174b1bf01fe11db70a9385eec88
Prashant79 <Prashaanttchaturvedi@gmail.com>
2023-07-31 13:37:42 +0000

    challenge completed

--- Challenges/solution_day1_script.sh
+++ Challenges/solution_day1_script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# First line of the script is the shebang which tells the system how to execute
+# This is a comment
 
 # Task  2: Echo
-echo "Scripting is fun with @TWS"
+echo "This is day 1 challenge"
 
 # Task  3: Variables
 variable1="Hello"
-variable2="Bash"
+variable2="World"
+greeting="$variable1, $variable2"
 
-# Task  4: Using Variables
-greeting="$variable1, $variable2!"
-echo "$greeting Welcome to the world of Bash scripting!"
+# Task  4: Using Variables to take input and print sum of two numbers
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+ 
+sum=$(( $num1 + $num2 ))
+ 
+echo "Sum is: $sum"
 
 # Task  5: Using Built-in Variables
 echo "My current bash path - $BASH"
@@ -20,10 +25,11 @@ echo "PID of bash I am running - $$"
 echo "My home directory - $HOME"
 echo "Where am I currently? - $PWD"
 echo "My hostname - $HOSTNAME"
+echo "This is the colon-separated list of directories where shell will look for commands - $PATH"
 
 # Task  6: Wildcards
-echo "Files with .txt extension in the current directory:"
-ls *.txt
+echo "Files with .sh extension in the current directory:"
+ls *.sh
 
 
 
